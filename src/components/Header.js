2af74c4a98f96a0bbd0f1483113c4b5b719943ee
gera garcia <ggeraweb@gmail.com>
2022-04-18 03:27:59 -0300

    header sin la imagen

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,19 +16,18 @@ function Header(){
       <Circle position="absolute" bg="blue.100" opacity="0.1" w="225px" h="225px" alignSelf="flex-end"/>
 
       {/* config por si la pantalla es small */}
-      <Flex direction={isNotSmallerScreen ? "row" : "columns"} spacing="200px" p={isNotSmallerScreen ? 18 : "0"} alignSelf="flex-start"></Flex>
+      <Flex mt={2} direction={isNotSmallerScreen ? "row" : "columns"} spacing="200px" p={isNotSmallerScreen ? "32" : "0"} alignSelf="flex-start">
 
-      {/* caja estilizada q contiene el texto */}
-      <Box mt={isNotSmallerScreen ? '0' : 18} align='flex-start'>
-        <Text fontSize="5xl" fontWeight="semibold">Hi, i am</Text>
-        {/* en este texto agrego un gradiente 'to right' con tres colores. bgClip ->adhiere el gradiente al texto */}
-        <Text fontSize="6xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text">Gerardo Garcia</Text>
-        {/* descripción */}
-        <Text fontSize="2xl"  color={isDark ? "gray.200" : "gray.500"}>Bienvenido a mi portfolio</Text>
-        <Button mt={8} colorScheme="blue" onClick={() => window.open("https://www.linkedin.com/in/gera-garcia")}>Here me</Button>
-      </Box>
-      
-      <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"} mb={isNotSmallerScreen ? "0" : "12"} borderRadius='full' backgroundColor="transparent" boxShadow="lg" boxSize="225px" src="https://bit.ly/imag_portfolio_glitch"></Image>
+        {/* caja estilizada q contiene el texto */}
+        <Box mt={isNotSmallerScreen ? "0" : 16} align='flex-start'>
+          <Text fontSize="5xl" fontWeight="semibold">Hi, i am</Text>
+          {/* en este texto agrego un gradiente 'to right' con tres colores. bgClip ->adhiere el gradiente al texto */}
+          <Text fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text">Gerardo Garcia</Text>
+          {/* descripción */}
+          <Text fontSize="1xl" color={isDark ? "gray.200" : "gray.500"}>Bienvenido a mi portfolio. He realizado este portfolio usando la liberería Chakra-UI de React</Text>
+          <Button mt={8} colorScheme="blue" onClick={() => window.open("https://www.linkedin.com/in/gera-garcia")}>Here me</Button>
+        </Box>
+      </Flex>
     </Stack>
     
   )
