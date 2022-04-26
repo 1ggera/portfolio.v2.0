ee324389836b6fded9c3c8f8d5c3ee0fe484c35f
gera garcia <ggeraweb@gmail.com>
2022-04-26 01:35:17 -0300

    description

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ function Header(){
           <Text fontSize="5xl" fontWeight="bold" color="cyan.500" bgGradient="linear(to-r, cyan.400, blue.600, purple.800)" bgClip='text'>Gerardo Garcia</Text>
 
           {/* descripción */}
-          <Text color={isDark ? "gray.200" : "gray.500"} fontSize="2xl">Welcome user. You are not a robot.<br/>Escribeme si necesitas alguna herramienta digital. <br/></Text>
+          <Text color={isDark ? "gray.200" : "gray.500"} fontSize="2xl">Welcome user. You are not a robot<br/>Escribeme si necesitas alguna herramienta digital<br/></Text>
 
           <Button mt={8} colorScheme="blue" onClick={() => window.open("https://www.linkedin.com/in/gera-garcia")}>Hire me</Button>
         </Box>
