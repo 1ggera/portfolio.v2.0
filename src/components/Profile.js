0cac475daa8be9f371d4e2d5f5c5efdf612158ed
gera garcia <ggeraweb@gmail.com>
2022-05-03 09:43:33 -0300

    centrar cards en moviles

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,30 +11,28 @@ function Profile(){
   return (
     <Flex direction={isNotSmallerScreen ? "row" : "column"} w="100%" maxWidth={{ base:"100vh", md: "130vh", lg:"130vh", xl: "130h" }} ml={"1"}>
       <Box alignSelf="center" px="10" py="16">
-        <Heading fontWeight="extrabold" color="cyan.500" size="4xl">
-          +2
-        </Heading>
+        <Heading fontWeight="extrabold" color="cyan.500" size="4xl">+2</Heading>
         <Text fontSize="2xl" color="gray.400">Years of experience</Text>
       </Box>
       <Box alignSelf="center" px="14" py="16">
         <Text fontWeight="bold" fontSize="2xl">Student of Programming of Informatics Systems and developer, specialised in movile first design.
         </Text>
-        <Flex direction={isNotSmallerScreen ? "row" : "column"} mt={8}>
-          <Flex rounded="xl" direction="column" mt={4} bg="blue.400" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: "cyan.400", }}>
+        <Flex direction={isNotSmallerScreen ? "row" : "column"} mt={8} >
+          <Flex alignSelf="center" rounded="xl" direction="column" mt={4} bg="blue.400" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: "cyan.400", }}>
           <Icon color="white" p="4" as={DiAndroid} w="24" h="24" ></Icon>
             <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
               Android Apps
             </Text>
           </Flex>
 
-          <Flex rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0} bg="blue.400" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: "cyan.400", }}>
+          <Flex alignSelf="center" rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0} bg="blue.400" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: "cyan.400", }}>
             <Icon color="white" p="4" as={DiCodeigniter} w="24" h="24"></Icon>
             <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
               Web Apps
             </Text>
           </Flex>
 
-          <Flex rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0} bg="blue.400" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: "cyan.400", }}>
+          <Flex alignSelf="center" rounded="xl" direction="column" mt={4} ml={isNotSmallerScreen ? 4 : 0} bg="blue.400" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: "cyan.400", }}>
           <Icon as={DiWebplatform} color="white" h="24" w="24" p="4"></Icon>
             <Text color="black" p="4" fontSize="xl" fontWeight="semibold">
               Web platforms
