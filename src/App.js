0d2276f70ab09f79453385504ee5f73f9b5bdbd3
gera garcia <ggeraweb@gmail.com>
2022-10-01 00:10:17 -0300

    chek buttons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Profile from "./components/Profile";
 import Social from "./components/Social";
 import Footer from "./components/Footer";
+import Footer from "./components/Social";
 
 function App() {
 
